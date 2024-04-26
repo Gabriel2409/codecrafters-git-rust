ffda10db945ae68c8601d72ce1f7d47933373557
Gabriel2409 <gabriel.cournelle@gmail.com>
2024-04-26 10:22:15 +0200

    Use of arg groups

--- src/main.rs
+++ src/main.rs
@@ -27,26 +27,31 @@ enum Commands {
         #[arg(
             short,
             help = "Pretty-print the contents of <object> based on its type.",
-            conflicts_with_all = ["exit_with_zero_status_if_exists", "type_obj", "size"]
+            group = "only_accept_hash", // args part of the same group are incompatible
+            conflicts_with = "allow_unkown_type" // did not find a better way. 
+            // TODO: Investigate how to make two arg groups incompatible
         )]
         pretty_print: bool,
 
         #[arg(
             short,
             help = "Exit with zero status if <object> exists and is a valid object",
-            conflicts_with_all = ["pretty_print", "type_obj", "size"],
+            group = "only_accept_hash",
+            conflicts_with = "allow_unkown_type" // did not find a better way. 
         )]
         exit_with_zero_status_if_exists: bool,
 
         #[arg(
             short,
             help = "Instead of the content, show the object type identified by <object>.",
-            conflicts_with_all = ["exit_with_zero_status_if_exists", "pretty_print", "size"],
+            group = "allow_unkown_type", // not implemented but follows more closely real git cat-file
+            conflicts_with = "only_accept_hash" // did not find a better way. 
         )]
         type_obj: bool,
 
         #[arg(short, help = "Instead of the content, show the <object> size",
-            conflicts_with_all = ["exit_with_zero_status_if_exists", "type_obj", "pretty_print"],
+            group = "allow_unkown_type",
+            conflicts_with = "only_accept_hash" // did not find a better way. 
 
         )]
         size: bool,
@@ -63,7 +68,7 @@ fn main() -> Result<()> {
     // matches just as you would the top level cmd
     match &cli.command {
         Commands::Init => git_init()?,
-        Commands::CatFile { .. } => git_init()?,
+        Commands::CatFile { .. } => git_cat_file(&vec!["".to_string()])?,
     };
     Ok(())
 }
