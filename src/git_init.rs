51e6e192ca8582ef77080601f0e0528de612f930
Gabriel2409 <gabriel.cournelle@gmail.com>
2024-04-26 10:10:17 +0200

    Start using clap for arg parsing

--- src/git_init.rs
+++ src/git_init.rs
@@ -2,14 +2,7 @@ use std::fs;
 
 use crate::{Error, Result};
 
-pub fn git_init(args: &[String]) -> Result<()> {
-    let nb_args = args.len();
-    if nb_args != 2 {
-        return Err(Error::InvalidNbArgs {
-            expected: 2,
-            got: nb_args,
-        });
-    }
+pub fn git_init() -> Result<()> {
     fs::create_dir(".git")?;
     fs::create_dir(".git/objects")?;
     fs::create_dir(".git/refs")?;
--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,61 @@ use git_cat_file::git_cat_file;
 use git_init::git_init;
 use std::env;
 
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about="Custom git", long_about=None )]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Create an empty Git repository.
+    Init,
+    ///  Provide content or type and size information for repository objects
+    CatFile {
+        #[arg(
+            short,
+            help = "Pretty-print the contents of <object> based on its type.",
+            conflicts_with_all = ["exit_with_zero_status_if_exists", "type_obj", "size"]
+        )]
+        pretty_print: bool,
+
+        #[arg(
+            short,
+            help = "Exit with zero status if <object> exists and is a valid object",
+            conflicts_with_all = ["pretty_print", "type_obj", "size"],
+        )]
+        exit_with_zero_status_if_exists: bool,
+
+        #[arg(
+            short,
+            help = "Instead of the content, show the object type identified by <object>.",
+            conflicts_with_all = ["exit_with_zero_status_if_exists", "pretty_print", "size"],
+        )]
+        type_obj: bool,
+
+        #[arg(short, help = "Instead of the content, show the <object> size",
+            conflicts_with_all = ["exit_with_zero_status_if_exists", "type_obj", "pretty_print"],
+
+        )]
+        size: bool,
+
+        #[arg(help = "hash corresponding to a given git <object>")]
+        hash: String,
+    },
+}
+
 fn main() -> Result<()> {
-    // Uncomment this block to pass the first stage
-    let args: Vec<String> = env::args().collect();
+    let cli = Cli::parse();
 
-    if args.is_empty() {
-        Err(Error::NoArgs)?;
-    }
-
-    match args[1].as_str() {
-        "init" => git_init(&args)?,
-        "cat-file" => git_cat_file(&args)?,
-        x => Err(Error::UnknownCommand(x.to_owned()))?,
-    }
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level cmd
+    match &cli.command {
+        Commands::Init => git_init()?,
+        Commands::CatFile { .. } => git_init()?,
+    };
     Ok(())
 }
