9562cd5afdac2a40e99e9d2c9a77902f55599dfd
Gabriel2409 <gabriel.cournelle@gmail.com>
2024-04-30 23:21:49 +0200

    WIP parse packfile

--- src/git_clone.rs
+++ src/git_clone.rs
@@ -1,4 +1,7 @@
-use std::{io::Read, path::Path};
+use std::{
+    io::{BufReader, Read},
+    path::Path,
+};
 
 use crate::{Error, Result};
 
@@ -89,7 +92,68 @@ pub fn git_clone<P: AsRef<Path>>(repository_url: &str, directory: P) -> Result<(
         .body(pack_content)
         .send()?;
 
-    println!("{}", res.text()?);
+    // res starts with 0008NAK\nPACK
+    let mut buf = vec![0; 12];
+    res.read_exact(&mut buf)?;
+    let val = String::from_utf8(buf).map_err(|e| Error::InvalidSmartHttpRes)?;
+    if val != "0008NAK\nPACK" {
+        return Err(Error::InvalidSmartHttpRes);
+    }
+
+    // then 4 bytes containing the version number
+    // for ex [0,0,0,2]
+    let mut buf = vec![0; 4];
+    res.read_exact(&mut buf)?;
+
+    // then 4 bytes containing the number of objects in the pack
+    let mut buf = vec![0; 4];
+    res.read_exact(&mut buf)?;
+    let nb_objects = buf[3] + buf[2] * 2 + buf[1] * 4 + buf[0] * 8;
+    dbg!(nb_objects);
+
+    // then the packfile itself
+    let mut buf = vec![0];
+    res.read_exact(&mut buf)?;
+    let cur_byte = buf[0];
+
+    // 1st bit should be 1 (MSB)
+    if cur_byte < 128 {
+        return Err(Error::InvalidSmartHttpRes);
+    }
+
+    // valid object types
+    // - OBJ_COMMIT (1) - OBJ_TREE (2) - OBJ_BLOB (3) - OBJ_TAG (4) - OBJ_OFS_DELTA (6) - OBJ_REF_DELTA (7)
+    let object_type = cur_byte >> 4 & 0b0111;
+    dbg!(object_type);
+
+    // then next 3 bits are part of size
+    let mut cur_size = cur_byte & 0b111;
+
+    //TODO:
+    loop {
+        println!("A");
+        res.read_exact(&mut buf)?;
+        let cur_byte = buf[0];
+        // if the MSB is 0, then the next bits are part of the size
+        // and then the rest encodes the data
+        if cur_byte < 128 {
+            cur_size += cur_byte << 4;
+            break;
+        // if the MSB is 1
+        } else {
+            cur_size += (cur_byte & 0b01111111) << 4;
+        }
+    }
+
+    dbg!(cur_size);
+
+    let mut buf = vec![0; cur_size as usize];
+    let mut buf = vec![0; 163];
+    res.read_exact(&mut buf)?;
+    let mut z = flate2::bufread::ZlibDecoder::new(&buf[..]);
+    let mut s = String::new();
+    z.read_to_string(&mut s)?;
+    dbg!(s);
 
     Ok(())
 }
